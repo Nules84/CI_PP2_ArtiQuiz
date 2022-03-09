e8e74c4798cebfff2df3af619726eaed117f34b8
Nuala King <nualagallagher84@hotmail.com>
2022-03-09 20:14:44 +0000

    moved completed javscript file into questions js file

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -150,3 +150,12 @@ decreaseScore = function (num) {
         tallyS.innerText = score;
     }
 }
+const name = document.getElementById("name");
+const saveScore = document.getElementById("saveScore");
+const endScore = document.getElementById("endScore");
+const newScore = localStorage.getItem("newScore");
+const totalResults = 10;
+const results = JSON.parse(localStorage.getItem("results")) || [];
+
+// This text will display on completed.html page
+endScore.innerText = "You Scored:" + " " + newScore + "!";
