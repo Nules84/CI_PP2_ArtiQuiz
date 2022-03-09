476d10e7dfd73e7a0ed8b7407d58d123079765b1
Nuala King <nualagallagher84@hotmail.com>
2022-03-09 22:06:40 +0000

    reverted back to separate js file

--- assets/js/completed.js
+++ assets/js/completed.js
@@ -1,4 +1,9 @@
+const name = document.getElementById("name");
+const saveScore = document.getElementById("saveScore");
 const endScore = document.getElementById("endScore");
+const newScore = localStorage.getItem("newScore");
+const totalResults = 10;
+const results = JSON.parse(localStorage.getItem("results")) || [];
 
 // This text will display on completed.html page
-endScore.innerText = "You Scored:" + " " + score + "!";
+endScore.innerText = "You Scored:" + " " + newScore + "!";
