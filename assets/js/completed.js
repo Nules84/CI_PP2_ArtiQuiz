c57f28e70a9b1965dd4508f4919640e5d18c4563
Nuala King <nualagallagher84@hotmail.com>
2022-03-09 21:28:49 +0000

    reverted back to two separate js files for completed js

---
+++ assets/js/completed.js
@@ -0,0 +1,4 @@
+const endScore = document.getElementById("endScore");
+
+// This text will display on completed.html page
+endScore.innerText = "You Scored:" + " " + score + "!";
--- assets/js/questions.js
+++ assets/js/questions.js
@@ -150,8 +150,3 @@ decreaseScore = function (num) {
         tallyS.innerText = score;
     }
 };
-
-const endScore = document.getElementById("endScore");
-
-// This text will display on completed.html page
-endScore.innerText = "You Scored:" + " " + score + "!";
