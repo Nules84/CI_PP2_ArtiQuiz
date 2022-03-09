141dabe7fc8720b6b5c419d9baa1abc860027fc3
Nuala King <nualagallagher84@hotmail.com>
2022-03-09 21:07:04 +0000

    edit testing file

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -104,7 +104,7 @@ countDown = function () {
         decreaseScore(incorrectPoints);
     }
     else (time = time - 1)
-}
+};
 
 setInterval("countDown()", 1000);
 
@@ -149,13 +149,9 @@ decreaseScore = function (num) {
         score -= num;
         tallyS.innerText = score;
     }
-}
-const name = document.getElementById("name");
-const saveScore = document.getElementById("saveScore");
+};
+
 const endScore = document.getElementById("endScore");
-const newScore = localStorage.getItem("newScore");
-const totalResults = 10;
-const results = JSON.parse(localStorage.getItem("results")) || [];
 
 // This text will display on completed.html page
-endScore.innerText = "You Scored:" + " " + newScore + "!";
+endScore.innerText = "You Scored:" + " " + score + "!";
